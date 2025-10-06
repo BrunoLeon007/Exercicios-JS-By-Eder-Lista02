344a9037030e3e27aaa34e677aec7928810c24ab
BrunoLeon007 <brunob734@gmail.com>
2025-10-06 20:53:15 -0300

    Add files via upload

--- script12.js
+++ script12.js
@@ -1,7 +1,9 @@
 var produto = Number(prompt("Digite o codigo do produto:"));
+var produto2 = Number(prompt("Digite o codigo do produto:"));
 var quantidade = Number(prompt("Digite a quantidade do produto:"));
 var produtoPreco= 10;
 var produto2Preco= 15; 
+
 
 if (produto === 1) {
     var total = quantidade * produtoPreco;
